65080d6a615ca12cec4e15efa389ff681e98952a
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-10-09 22:43:27 +0530

    create a nextj server to get access to custom routes

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,30 +1,42 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
-import { Button, Form, Input } from "semantic-ui-react";
+import { Button, Form, Input, Message } from "semantic-ui-react";
 import factory from "../../etherum/factory";
 import web3 from "../../etherum/web3";
 
 class NewCampaign extends Component {
   state = {
     minimumContribution: "",
+    errorMessage: "",
+    loading: false,
   };
 
   onSubmit = async (event) => {
     event.preventDefault();
 
-    window.ethereum.request({ method: "eth_requestAccounts" });
-    const accounts = await web3.eth.getAccounts();
+    this.setState({ loading: true, errorMessage: "" });
 
-    await factory.methods.createCampaign(this.state.minimumContribution).send({
-      from: accounts[0],
-    });
+    try {
+      window.ethereum.request({ method: "eth_requestAccounts" });
+      const accounts = await web3.eth.getAccounts();
+
+      await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({
+          from: accounts[0],
+        });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
   };
 
   render() {
     return (
       <Layout>
         <h3>Create a new Campaign</h3>
-        <Form onSubmit={this.onSubmit}>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input
@@ -36,8 +48,9 @@ class NewCampaign extends Component {
               }
             />
           </Form.Field>
-          <Button type="submit" primary>
-            Submit
+          <Message error header="Oops!!" content={this.state.errorMessage} />
+          <Button type="submit" primary loading={this.state.loading}>
+            Create
           </Button>
         </Form>
       </Layout>
