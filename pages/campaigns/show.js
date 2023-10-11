5aecbd4a4747a38e512018eedddc14c0be6ea125
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-10-11 11:14:30 +0530

    fetch campaign data and display it using Card.Group

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Layout from "../../components/Layout";
 import Campaign from "../../etherum/campaign";
 import { Card } from "semantic-ui-react";
+import web3 from "../../etherum/web3";
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -30,13 +31,36 @@ class CampaignShow extends Component {
       {
         header: manager,
         description:
-          "The manager created this Camapign and can create requests for withdraw money from the campaign",
-        meta: "Address of the manager",
-        Style: { overflowWrap: "break-word" },
+          "The manager created this camapign and can create requests for withdraw money from the campaign.",
+        meta: "Address of the Manager",
+        style: { overflowWrap: "break-word" },
+      },
+      {
+        header: minimumContribution,
+        description:
+          "You must contribute at least this much wei to become an approver.",
+        meta: "Minimum Contribution (wei)",
+      },
+      {
+        header: requestCount,
+        description:
+          "A request tries to withdarw money from the contract.request must be approved by the approvers.",
+        meta: "Number of Requests",
+      },
+      {
+        header: approversCount,
+        description: "Number of people that already donated to this campaign.",
+        meta: "Number of Approvers",
+      },
+      {
+        header: web3.utils.fromWei(balance, "ether"),
+        description:
+          "the balance is how much money this campaign has left to spend",
+        meta: "Campaign Balance (ether)",
       },
     ];
 
-    <Card.Group items={items} />;
+    return <Card.Group items={items} />;
   }
 
   render() {
