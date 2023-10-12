c8407fa2cfe5537978973614ea107dfbd127361e
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-10-12 18:07:35 +0530

    get requests array using fetching individual request at a time

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
 import Layout from "../../../components/Layout";
+import Campaign from "../../../etherum/campaign";
 
 class Requests extends Component {
   static async getInitialProps(props) {
     const address = props.query.address;
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
 
-    return { address };
+    const requestsArr = await Promise.all(
+      Array(requestCount)
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+
+    return { address, requestsArr, requestCount };
   }
 
   render() {
+    const { Header, HeaderCell, Row, Body } = Table;
+
     return (
       <Layout>
         <h3>This is Requests</h3>
--- pages/campaigns/requests/newRequest.js
+++ pages/campaigns/requests/newRequest.js
@@ -1,8 +1,95 @@
 import React, { Component } from "react";
+import Layout from "../../../components/Layout";
+import { Button, Message, Form, Input } from "semantic-ui-react";
+import web3 from "../../../etherum/web3";
+import Campaign from "../../../etherum/campaign";
+import { Router } from "../../../routes";
 
 class newRequest extends Component {
+  static async getInitialProps(props) {
+    const address = props.query.address;
+
+    const campaign = Campaign(props.query.address);
+
+    return { address, campaign };
+  }
+
+  state = {
+    description: "",
+    amount: "",
+    recipient: "",
+    errorMessage: "",
+    loading: false,
+  };
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const { campaign, address } = this.props;
+
+    this.setState({ loading: true, errorMessage: "" });
+
+    try {
+      window.ethereum.request({ method: "eth_requestAccounts" });
+      const accounts = await web3.eth.getAccounts();
+
+      await campaign.methods
+        .createRequest(
+          this.state.description,
+          web3.utils.toWei(this.state.amount, "ether"),
+          this.state.recipient
+        )
+        .send({
+          from: accounts[0],
+        });
+
+      Router.pushRoute(`/campaigns/${address}/requests`);
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
+  };
+
   render() {
-    return <h3>Create new Request</h3>;
+    return (
+      <Layout>
+        <h3>Create a Request</h3>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+          <Form.Field>
+            <label>Description</label>
+            <Input
+              value={this.state.description}
+              onChange={(event) => {
+                this.setState({ description: event.target.value });
+              }}
+            ></Input>
+          </Form.Field>
+          <Form.Field>
+            <label>Amount of ether</label>
+            <Input
+              value={this.state.amount}
+              onChange={(event) => {
+                this.setState({ amount: event.target.value });
+              }}
+            ></Input>
+          </Form.Field>
+          <Form.Field>
+            <label>Recipient</label>
+            <Input
+              value={this.state.recipient}
+              onChange={(event) => {
+                this.setState({ recipient: event.target.value });
+              }}
+            ></Input>
+          </Form.Field>
+          <Message error header="Oops!!" content={this.state.errorMessage} />
+          <Button type="submit" primary loading={this.state.loading}>
+            Create
+          </Button>
+        </Form>
+      </Layout>
+    );
   }
 }
 
